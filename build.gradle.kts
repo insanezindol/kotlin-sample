d39acca733e46038350dc1a3013b52a0f6e5f64f
jhlee <leejinhyung90@gmail.com>
2022-05-12 21:13:55 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.6.7"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
+    kotlin("plugin.allopen") version "1.4.32"
+}
+
+// allOpen 정의
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.apache.httpcomponents:httpclient:4.5")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/example/sample/RepositoriesTests.kt
@@ -0,0 +1,37 @@
+package com.example.sample
+
+import com.example.sample.entities.Article
+import com.example.sample.entities.User
+import com.example.sample.repositories.ArticleRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.data.repository.findByIdOrNull
+
+@DataJpaTest
+class RepositoriesTests @Autowired constructor(
+        val entityManager: TestEntityManager,
+        val articleRepository: ArticleRepository
+){
+
+    @Test
+    fun `When findByIdOrNull then return Article`() {
+        // given
+        val newUser = User("s2moon98", "jieun", "moon")
+        entityManager.persist(newUser)
+
+        val newArticle = Article("new article title", "new headline", "new content", newUser)
+        entityManager.persist(newArticle)
+
+        entityManager.flush()
+
+        // when
+        // !!의 의미 : null이 들어올 수 없는 경우
+        val found = articleRepository.findByIdOrNull(newArticle.id!!)
+
+        // then
+        assertThat(found).isEqualTo(newArticle)
+    }
+}
